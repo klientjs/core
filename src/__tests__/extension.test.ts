24c86cc25cce45658f08036a18d161e1fece91f9
Brandon <brandon.marcachi@acseo-conseil.fr>
2023-07-22 18:29:24 +0200

    test: add test case for property consumed directly in bag

--- src/__tests__/extension.test.ts
+++ src/__tests__/extension.test.ts
@@ -34,6 +34,7 @@ test('extension:load', () => {
 
   expect(klient.extensions.includes('@klient/test')).toBe(true);
   expect(klient.services.get('test')).toBeDefined();
+  expect(klient.services.test).toBeDefined();
   expect(klient.parameters.get('test')).toBe(true);
   expect(klient.writable).toBe('something');
 
